46b74bd647a4d3f1aae754135d113053e94ccbab
Amin Saffarnejad <vira1368@gmail.com>
2022-08-28 18:34:55 +0430

    Receive message from queue implemented

--- ProductConsole/Program.cs
+++ ProductConsole/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var connectionFactory = new ConnectionFactory
+{
+    HostName = "localhost"
+};
+var connection = connectionFactory.CreateConnection();
+var channel = connection.CreateModel();
+channel.QueueDeclare("product", exclusive: false);
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Product message received: {message}");
+};
+channel.BasicConsume("product", true, consumer);
+Console.ReadKey();
