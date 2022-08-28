57031adca988cbbc3d4c906bb2adef70a68c1766
Amin Saffarnejad <vira1368@gmail.com>
2022-08-28 12:14:31 +0430

    Product class created.

---
+++ ProductAPI/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
