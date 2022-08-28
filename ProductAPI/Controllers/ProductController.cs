fc29edd7a95d17282125453cf6406b9794a58d3a
Amin Saffarnejad <vira1368@gmail.com>
2022-08-28 15:25:18 +0430

    Product controller created.

---
+++ ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Models;
+using ProductAPI.RabbitMQ;
+using ProductAPI.Services;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly IProducer _producer;
+
+        public ProductController(IProductService productService, IProducer producer)
+        {
+            _productService = productService;
+            _producer = producer;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return _productService.GetProducts();
+        }
+
+        [HttpGet("{id}")]
+        public Product GetProductById(int id)
+        {
+            return _productService.GetProductById(id);
+        }
+
+        [HttpPost]
+        public Product AddProduct(Product product)
+        {
+            var productData = _productService.AddProduct(product);
+            _producer.SendMessage(productData);
+            return productData;
+        }
+
+        [HttpPut]
+        public Product UpdateProduct(Product product)
+        {
+            return _productService.UpdateProduct(product);
+        }
+
+        [HttpDelete]
+        public bool DeleteProduct(int id)
+        {
+            return _productService.DeleteProduct(id);
+        }
+    }
+}
