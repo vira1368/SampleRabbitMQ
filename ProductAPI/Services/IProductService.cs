f1f3b52a8a609ba31741d18e5251dbea858d8c7c
Amin Saffarnejad <vira1368@gmail.com>
2022-08-28 12:39:19 +0430

    IProductService interface created.

---
+++ ProductAPI/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using ProductAPI.Models;
+
+namespace ProductAPI.Services
+{
+    public interface IProductService
+    {
+        public IEnumerable<Product> GetProducts();
+        public Product GetProductById(int id);
+        public Product AddProduct(Product product);
+        public Product UpdateProduct(Product product);
+        public bool DeleteProduct(int id);
+    }
+}
