28735667d11dc857ebb4133a86e35f680107cd34
Amin Saffarnejad <vira1368@gmail.com>
2022-08-28 13:31:32 +0430

    Producer class created.

---
+++ ProductAPI/RabbitMQ/Producer.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace ProductAPI.RabbitMQ
+{
+    public class Producer : IProducer
+    {
+        public void SendMessage<T>(T message)
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("product", exclusive: false);
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+            channel.BasicPublish(exchange: "", routingKey: "product", body: body);
+        }
+    }
+}
