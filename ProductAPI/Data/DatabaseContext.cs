9626c30c8eaf32b018b5b467aba3b12971b98f02
Amin Saffarnejad <vira1368@gmail.com>
2022-08-28 12:22:25 +0430

    DatabaseContext class created.

---
+++ ProductAPI/Data/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Models;
+
+namespace ProductAPI.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        protected readonly IConfiguration _configuration;
+
+        public DatabaseContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DatabaseConnection"));
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
